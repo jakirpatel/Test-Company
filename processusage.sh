438c68a0426d1cc6842c98e01fe70c2fb6750f03
jakirpatel <31799856+jakirpatel@users.noreply.github.com>
2017-09-30 20:41:24 +0530

    Update processusage.sh

--- processusage.sh
+++ processusage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-processusage="/Users/jakirpatel/Desktop/Test/file.txt"
+processusage="file.txt"
 function getUsage(){
 
     if [[ -f "$1" ]]
@@ -9,12 +9,8 @@ function getUsage(){
         #echo $ip $user $pass $process
         sshpass -p "$pass" ssh $user@$ip  -o StrictHostKeyChecking=no -T " 
         case "$OSTYPE" in
-        darwin*)  
-            echo $ip $process $(top -pid $(ps -A | grep -m1 $process | awk '{print $1}') -stats MEM -l 1 | grep -A1 'MEM'| grep -v "MEM")
-            exit 1
-            ;;
         linux*)
-            echo $ip $process $(ps -p $process -F --no-headers | awk '{print $6}')"kb"
+            echo $ip $process $(top -b -n1 | grep $process | head -1 | awk '{print $6}')"kb"
             exit 1
 	    ;;
         *)
